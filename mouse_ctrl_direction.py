bf2856e83eb51d11e5fafe9895f88a09a77eed7e
yanming1560 <36906575+yanming1560@users.noreply.github.com>
2018-08-16 15:03:54 +0800

    Update mouse_ctrl_direction.py

--- mouse_ctrl_direction.py
+++ mouse_ctrl_direction.py
@@ -6,12 +6,24 @@ pag.click(sx/2,sy/2)
 
 while True:
     x,y=pag.position()
-    if sx/3<x<sx*2/3 and 0<=y<sy/3:
+    if sx/3<x<sx*2/3 and 0<y<sy/3:
         pag.press('up')
-    elif sx/3<x<sx*2/3 and sy*2/3<y<=sy:
+    elif sx/3<x<sx*2/3 and sy*2/3<y<sy:
         pag.press('down')
-    elif 0<=x<sx/3 and sy/3<y<sy*2/3:
+    elif 0<x<sx/3 and sy/3<y<sy*2/3:
         pag.press('left')
-    elif sx*2/3<x<=sx and sy/3<y<sy*2/3:
+    elif sx*2/3<x<sx and sy/3<y<sy*2/3:
+        pag.press('right')
+    elif 0<x<sx/3 and 0<y<sy/3:
+        pag.press('up')
+        pag.press('left')
+    elif sx*2/3<x<sx and 0<y<sy/3:
+        pag.press('up')
+        pag.press('right')
+    elif 0<x<sx/3 and sy*2/3<y<sy:
+        pag.press('down')
+        pag.press('left')
+    elif sx*2/3<x<sx and sy*2/3<y<sy:
+        pag.press('down')
         pag.press('right')
     time.sleep(0.3)
