e6000933f11b8730cd50bf6fd7720fb146a2cff1
yanming1560 <36906575+yanming1560@users.noreply.github.com>
2018-12-07 14:58:33 +0800

    Update itchat_test.py

--- itchat_test.py
+++ itchat_test.py
@@ -8,8 +8,12 @@ def simple(msg):                #子函数竟然是自动运行的。。。
     if msg['Type'] == TEXT:     #如果是文本类型的消息输入
         print(msg['User'])      #消息里面User相关的内容
         all='你的昵称：'+msg['User']['NickName']+'\n'
-        all+='性别:'+'男' if msg['User']['Sex']==1 else '女'
-        all+='\n'
+        if msg['User']['Sex']==0:
+            all += '性别:未知\n'
+        elif msg['User']['Sex']==1:
+            all += '性别:男\n'
+        elif msg['User']['Sex']==2:
+            all+='性别:女\n'
         all+='地址：'+msg['User']['Province']+msg['User']['City']
         print(all)
         itchat.send_msg(all,toUserName=msg['FromUserName'])     #消息回复
